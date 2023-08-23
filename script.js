1dfc17d455c19e5535a9dc0a3956fd8a53b84afc
Marinalm15 <marinalm15@hotmail.com>
2023-08-23 19:37:52 -0300

    continuação da lógica: marcar respostas certas  e contar pontos

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //declaração de variáveis
 const question = document.querySelector("#question");
 const answersBox = document.querySelector("#answers-box");
-const quizzContainer = document.querySelector("#quiz-container");
+const quizzContainer = document.querySelector("#quizz-container");
 const scoreContainer = document.querySelector("#score-container");
 const letters = ["a", "b", "c", "d"];
 let points = 0;
@@ -92,6 +92,71 @@ function createQuestion(i) {
 
     questionText.textContent = questions[i].question;
     questionNumber.textContent = i + 1;
+
+    //insere as alternativas
+    questions[i].answers.forEach(function(answer, i) {
+    
+        // Altera texto do template
+        const answerTemplate = document.querySelector(".answer-template").cloneNode(true);
+    
+        const letterBtn = answerTemplate.querySelector(".btn-letter");
+        const answerText = answerTemplate.querySelector(".question-answer");
+    
+        letterBtn.textContent = letters[i];
+        answerText.textContent = answer['answer'];
+    
+        answerTemplate.setAttribute("correct-answer", answer["correct"]);
+    
+        // remove classe de hide e template do template
+        answerTemplate.classList.remove("hide");
+        answerTemplate.classList.remove("answer-template");
+    
+        // Insere template na tela
+        answersBox.appendChild(answerTemplate);
+
+        //inserir evento click 
+        answerTemplate.addEventListener("click", function() {
+            console.log(this);
+        })
+
+      });
+
+      //incrementar o número da questão
+      actualQuestion++;
+}
+
+//verificando resposta do usuário
+function checkAnswer(btn, buttons) {
+  
+    // Exibir respostas erradas e a certa
+    buttons.forEach(function(button) {
+  
+      if(button.getAttribute("correct-answer") === "true") {
+        button.classList.add("correct-answer");
+        // checa se o usuário acertou
+        if(btn === button) {
+          // incrementa os pontos
+          points++;
+        }
+      } else {
+        button.classList.add("wrong-answer");
+      }
+  
+    });
+  
+    nextQuestion();
+  
+}
+
+function nextQuestion() {
+    setTimeout(function() {
+        if(actualQuestion >= question.length) {
+
+        }
+
+        createQuestion(actualQuestion);
+        
+    }, 1500)
 }
 
 init();
